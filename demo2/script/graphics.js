ec4f45380f23e28195e0eee11b157b96281b4b09
Adam Pearson <ampearson@gmail.com>
2010-10-10 22:56:27 -0700

    Turns out shadows really don't work with Chrome, also, use radians, doy...

--- demo2/script/graphics.js
+++ demo2/script/graphics.js
@@ -1,4 +1,6 @@
 const FPS = 30;
+const SECONDSBETWEENFRAMES = 1/FPS;
+
 var context
 var mySquare;
 
@@ -7,7 +9,7 @@ var initGraphics = function(){
   context = canvas.getContext('2d');
   mySquare = square(context);
 
-  setInterval(function(){ draw(context); }, 1000 / FPS)
+  setInterval(function(){ draw(context); }, SECONDSBETWEENFRAMES * 1000)
 };
 
 var draw = function(context){
@@ -20,46 +22,41 @@ var square = function(context){
   var that = {};
   var canvasWidth = context.canvas.width,
       canvasHeight = context.canvas.height,
-      iterations = 0,
+      currentTime = 0,
       width = 100,
       height = 100,
-      red = 250,
-      speed = 1;
+      rotation = 0,
+      speed = (Math.PI * 2) / (FPS * 2);
 
   that.draw = function(){
+    currentTime += SECONDSBETWEENFRAMES;
+
     context.save();
     context.translate((canvasWidth / 2), (canvasHeight / 2))
     context.rotate(angle());
-    context.shadowOffsetX = 20.0;
-    context.shadowOffsetY = 5.0;
-    context.shadowBlur = 500.0;
-    context.shadowColor = "#630";
     context.translate(-(width / 2), -(height / 2));
     context.fillStyle = "rgb(:red, :green, 0)".replace(":red", red()).replace(":green", green())
     context.fillRect(0, 0, width, height)
     context.restore();
-    iterations += 1;
   };
 
   var red = function(){
-    var red =  Math.abs(Math.sin(iterations / 500)) * 255;
+    var red =  Math.abs(Math.sin(currentTime / 15)) * 255;
     red = Math.ceil(red);
     return red;
   }
 
   var green = function(){
-    var green = Math.abs(Math.sin(iterations / 200)) * 150;
+    var green = Math.abs(Math.sin(currentTime / 10)) * 150;
     green = Math.ceil(green);
     green += 50;
     return green;
   }
 
   var angle = function(){
-    // var sineWave = (Math.sin(iterations / FPS) + 1) / 2
-    var sineWave = Math.sin(iterations / 5000);
-    // console.log(Math.abs(sineWave));
-    var angle = Math.abs(sineWave) * 360;
-    return angle;
+    rotation += speed;
+    if(rotation > (Math.PI * 2)){ rotation = 0 }
+    return rotation;
   }
 
   return that;
